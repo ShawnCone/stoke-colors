c40b965de2b4b544e9604991c57b627dab86121b
ShawnCone <deandre.dave@gmail.com>
2024-03-28 16:18:19 -0700

    added env variable usage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import "../app/globals.css";
 
 const COLORS = ["#BCE5E8", "#3C4248", "#FBD5D7", "009DB5"];
 
+const DB_URL = import.meta.env.VITE_DB_URL as string;
+const DB_TOKEN = import.meta.env.VITE_DB_TOKEN as string;
+
+console.log({ DB_URL, DB_TOKEN });
+
 function App() {
   return (
     <main>
